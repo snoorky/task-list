3d372311e4c9ca351f57f8112031500a2894202a
sno-orky <raphaelvitorlopes@icloud.com>
2024-02-24 18:59:32 -0300

    task list completed

--- assets/js/control.js
+++ assets/js/control.js
@@ -1,50 +1,50 @@
-let input = document.getElementById('inputTarefa');
+let input = document.getElementById('inputTask');
 let btnAdd = document.getElementById('btnAdd');
-let main = document.getElementById('areaLista');
-let contador = 0;
+let main = document.getElementById('listArea');
+let counter = 0;
 
-function addTarefa() {
-    let valorInput = input.value;
+function addTask() {
+    let inputValue = input.value;
 
-    if ((valorInput !== "") && (valorInput !== null) && (valorInput !== undefined)) {
-        ++contador;
-        let novoItem = `<div id="${contador}" class="item">
-        <div onclick="marcarTarefa(${contador})" class="itemIcon">
-            <i id="icone${contador}" class="fi fi-rr-circle"></i>
+    if ((inputValue !== "") && (inputValue !== null) && (inputValue !== undefined)) {
+        ++counter;
+        let newItem = `<div id="${counter}" class="item">
+        <div onclick="markerTask(${counter})" class="itemIcon">
+            <i id="icon${counter}" class="fi fi-rr-circle"></i>
         </div>
-        <div onclick="marcarTarefa(${contador})" class="itemNome">
-            <p>${valorInput}</p>
+        <div onclick="markerTask(${counter})" class="itemName">
+            <p>${inputValue}</p>
         </div>
-        <div class="itemBotao">
-            <button onclick="deletar(${contador})" class="delete"><i class="fi fi-rr-trash"></i></button>
+        <div class="itemButton">
+            <button onclick="deleteTask(${counter})" class="delete"><i class="fi fi-rr-trash"></i></button>
         </div>
     </div>`;
 
-    main.innerHTML += novoItem
+    main.innerHTML += newItem
     input.value = "";
     input.focus();
     }
 }
 
-function deletar(id) {
+function deleteTask(id) {
     var tarefa = document.getElementById(id);
     tarefa.remove();
 }
 
-function marcarTarefa(id) {
+function markerTask(id) {
     var item = document.getElementById(id);
     var classe = item.getAttribute('class');
 
     if (classe == "item") {
-        item.classList.add('clicado');
-        var icone = document.getElementById('icone' + id);
+        item.classList.add('active');
+        var icone = document.getElementById('icon' + id);
         icone.classList.remove('fi-rr-circle');
         icone.classList.add('fi-rr-check-circle');
 
         item.parentNode.appendChild(item);
     } else {
-        item.classList.remove('clicado');
-        var icone = document.getElementById('icone' + id);
+        item.classList.remove('active');
+        var icone = document.getElementById('icon' + id);
         icone.classList.add('fi-rr-circle');
         icone.classList.remove('fi-rr-check-circle');
     }
